0ff7918faebf439461206d3836c87d5f38481d80
안은종 <aej9300@gmail.com>
2022-05-26 14:41:22 +0900

    list add시 중복되는 값 포인터 문제로 확인 수정

--- remind/src/main/java/com/remind/ej/memory/MemoryServiceImpl.java
+++ remind/src/main/java/com/remind/ej/memory/MemoryServiceImpl.java
@@ -19,12 +19,11 @@ public class MemoryServiceImpl implements MemoryService {
 		int result = 0;	
 		
 		ArrayList list = new ArrayList();
-		Map data = new HashMap();
-		
 		String[] wordlist = ((String) map.get("word")).split(","); // 111, 222, 333
 		String[] meanlist = ((String) map.get("mean")).split(","); // 444, 555, 666
 		
 		for(int i=0; i<wordlist.length; i++) {
+			Map data = new HashMap();
 			data.put("word", wordlist[i]); 
 			data.put("mean", meanlist[i]);
 			list.add(data);
